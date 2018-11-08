fc741369a0f2f4e6501bb83b7967ff37ff76478d
Wasif Khan <waasif.khaan@gmail.com>
2018-11-08 05:19:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function getFirstSelector(selector) {
 function nestedTarget(selector){
   return document.querySelector('#nested .target')
 }
+
+function deepestChild() {
+  return document.querySelectorAll('#grand-node').last
+}
