cf109f3f2ac916961c9e6393be2186171d053416
Wasif Khan <waasif.khaan@gmail.com>
2018-11-08 05:24:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function nestedTarget(selector){
 function deepestChild() {
   return document.querySelectorAll('#grand-node')
 }
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
